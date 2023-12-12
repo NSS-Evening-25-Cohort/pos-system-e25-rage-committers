3b6976c73f0ae8e36bb7a128a4db7b64a1857c09
MaxRubel <maxrubel1@gmail.com>
2023-12-12 11:29:09 -0600

    revenue page is calculating revenue

--- pages/revenuePage.js
+++ pages/revenuePage.js
@@ -1,5 +1,26 @@
+import { render } from 'sass';
+import getClosedOrders from '../api/orderData';
+import clearDom from '../utils/clearDom';
+import renderToDOM from '../utils/renderToDom';
+
 const revenuePage = () => {
-  console.warn('welcome to the revenue page');
+  clearDom();
+  const domString = '<div id="total-revenue"></div>';
+  renderToDOM('#view', domString);
+  getClosedOrders().then((closedOrderArray) => {
+    const orderTotalsArray = [];
+    closedOrderArray.forEach((order) => {
+      const singleTotal = order.total_amount;
+      orderTotalsArray.push(singleTotal);
+    });
+    const initialValue = 0;
+    const totalRevenue = orderTotalsArray.reduce(
+      (accumulator, currentValue) => accumulator + currentValue,
+      initialValue,
+    );
+    const domString2 = `You're total revenue is $${totalRevenue}`;
+    renderToDOM('#total-revenue', domString2);
+  });
 };
 
 export default revenuePage;
