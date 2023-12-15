e7a1325a46a9282c26d19c1b7ed684d5edc4682b
MaxRubel <maxrubel1@gmail.com>
2023-12-14 19:02:34 -0600

    add items page is working

--- events/domEvents.js
+++ events/domEvents.js
@@ -9,6 +9,14 @@ const domEvents = () => {
     if (e.target.id === 'create-new-order-form') {
       createEditOrderPage();
     }
+    if (e.target.id.includes('close-order')) {
+      // eslint-disable-next-line
+      const [, firebaseKey] = e.target.id.split('--');
+      // TATIANNA CALL THIS FUNCTION TO RENDER THE CLOSE ORDER PAGE
+      // THE FIREBASE KEY IS THE FIREBASE KEY OF THE ORDER YOU HAVE TO CLOSE OUT/UPDATE
+
+      // closeOrderPage(firebaseKey);
+    }
   });
 };
 
