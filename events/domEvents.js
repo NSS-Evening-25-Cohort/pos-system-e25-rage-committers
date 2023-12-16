d56c26c585e734995e4e1f49944be8aba2975906
MaxRubel <maxrubel1@gmail.com>
2023-12-16 12:25:29 -0600

    small pull fix thang

--- events/domEvents.js
+++ events/domEvents.js
@@ -1,18 +1,16 @@
 import revenuePage from '../pages/revenuePage';
 import createEditOrderPage from '../components/forms/createEditOrderPage';
 import showOrders from '../pages/viewOrdersPage';
-import { getOrders, deleteOrder } from '../api/orderData';
+import { getOrders, getSingleOrder, deleteOrder } from '../api/orderData';
 
 const domEvents = () => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
     if (e.target.id === 'view-orders-button') {
       getOrders().then(showOrders);
     }
-
     if (e.target.id === 'create-new-order-form') {
       createEditOrderPage({});
     }
-
     if (e.target.id === 'view-revenue-button') {
       revenuePage();
     }
@@ -20,7 +18,6 @@ const domEvents = () => {
       // const [, firebaseKey] = e.target.id.split('--');
       // TATIANNA CALL THIS FUNCTION TO RENDER THE CLOSE ORDER PAGE
       // THE FIREBASE KEY IS THE FIREBASE KEY OF THE ORDER YOU HAVE TO CLOSE OUT/UPDATE
-
       // closeOrderPage(firebaseKey);
     }
     if (e.target.id.includes('edit-order')) {
@@ -52,6 +49,7 @@ const domEvents = () => {
           console.log(orderObj); // Log the returned order object
           createEditOrderPage(orderObj);
         });
+    }
     if (e.target.id.includes('delete-order')) {
       const [, firebaseKey] = e.target.id.split('--');
       deleteOrder(firebaseKey).then(() => {
