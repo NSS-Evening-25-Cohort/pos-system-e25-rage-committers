384f123452635faed2ecf41e783df03accce7a1b
MaxRubel <maxrubel1@gmail.com>
2023-12-17 17:35:50 -0600

    fixed a small show orders error on the navBar

---
+++ components/forms/revenueFilterBar.js
@@ -0,0 +1,101 @@
+import renderToDOM from '../../utils/renderToDom';
+
+const revenueFilterBar = () => {
+  const domString = `
+  <form id="revenue-filter-bar-form" class="revenue-filter-bar-style">
+      <div id="month-container"></div>
+      </div>
+      <div class="submit-button-revenue-container">
+        <button type="submit" class="btn btn-secondary">Filter</button>
+      </div>
+    </form>`;
+  renderToDOM('#revenue-filter-bar', domString);
+
+//   const domStringMonth = `
+//   <div><label for="months">Month</label></div>
+//   <select id="month" name="month" ${document.getElementById('day').value ? '' : '/required'}>
+//     <option value="noValue">month</option>
+//     <option value="1">Jan</option>
+//     <option value="2">Feb</option>
+//     <option value="3">March</option>
+//     <option value="4">April</option>
+//     <option value="5">May</option>
+//     <option value="6">June</option>
+//     <option value="7">July</option>
+//     <option value="8">August</option>
+//     <option value="9">September</option>
+//     <option value="10">October</option>
+//     <option value="11">November</option>
+//     <option value="12">December</option>
+//   </select>
+//  `;
+//   renderToDOM('#month-container', domStringMonth);
+};
+
+export default revenueFilterBar;
+
+/* <div><label for="months">Month</label></div>
+<select id="month" name="month" ${document.getElementById('day').value === 'noValue' ? '/required' : ''}>
+  <option value="noValue">month</option>
+  <option value="1">Jan</option>
+  <option value="2">Feb</option>
+  <option value="3">March</option>
+  <option value="4">April</option>
+  <option value="5">May</option>
+  <option value="6">June</option>
+  <option value="7">July</option>
+  <option value="8">August</option>
+  <option value="9">September</option>
+  <option value="10">October</option>
+  <option value="11">November</option>
+  <option value="12">December</option>
+</select>
+</div> */
+/* <select id="month" name="month" ${document.getElementById('day').value === 'noValue' ? '/required' : ''}> */
+
+/* <div class="submit-button-revenue-container">
+<button type="submit" class="btn btn-secondary">Filter</button>
+</div> */
+
+/* <div id="year-container">
+        <div><label for="year">Year</label></div>
+        <input type="number" id="year" min="1900" max="2100" name="year" required/>
+</div> */
+
+/* <div id="day-container">
+<div><label for="day">Day</label></div>
+  <select id="day" name="day">
+    <option value="">day</option>
+    <option value="1">1</option>
+    <option value="2">2</option>
+    <option value="3">3</option>
+    <option value="4">4</option>
+    <option value="5">5</option>
+    <option value="6">6</option>
+    <option value="7">7</option>
+    <option value="8">8</option>
+    <option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
+    <option value="13">13</option>
+    <option value="14">14</option>
+    <option value="15">15</option>
+    <option value="16">16</option>
+    <option value="17">17</option>
+    <option value="18">18</option>
+    <option value="19">19</option>
+    <option value="20">20</option>
+    <option value="21">21</option>
+    <option value="22">22</option>
+    <option value="23">23</option>
+    <option value="24">24</option>
+    <option value="25">25</option>
+    <option value="26">26</option>
+    <option value="27">27</option>
+    <option value="28">28</option>
+    <option value="29">29</option>
+    <option value="30">30</option>
+    <option value="31">31</option>
+  </select>
+</div> */
